ee744fd356cc7af90a9e987b6d11efeabf7f70ab
Sven H <sven.hjol@protonmail.com>
2025-04-24 22:04:20 +0100

    Update to work with latest API

--- src/main/java/svenhjol/charmony/chorus_network/client/features/chorus_network/DarkModeProviders.java
+++ src/main/java/svenhjol/charmony/chorus_network/client/features/chorus_network/DarkModeProviders.java
@@ -2,20 +2,20 @@ package svenhjol.charmony.chorus_network.client.features.chorus_network;
 
 import net.minecraft.client.gui.screens.Screen;
 import svenhjol.charmony.api.Api;
-import svenhjol.charmony.api.UsesDarkMode;
+import svenhjol.charmony.api.DarkModeProvider;
 import svenhjol.charmony.core.base.Setup;
 import svenhjol.charmony.core.helpers.ColorHelper;
 
 import java.util.function.Predicate;
 
-public class DarkModeProviders extends Setup<ChorusNetwork> implements UsesDarkMode {
+public class DarkModeProviders extends Setup<ChorusNetwork> implements DarkModeProvider {
     public DarkModeProviders(ChorusNetwork feature) {
         super(feature);
         Api.registerProvider(this);
     }
 
     @Override
-    public Predicate<Screen> usingDarkMode() {
+    public Predicate<Screen> usesDarkMode() {
         return screen -> {
             if (screen instanceof ChannelScreen channelScreen) {
                 var color = channelScreen.getChannelColor();
